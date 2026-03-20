ab9e89dc65d83c5255556e25cefd7e4fc5cee6bb
pmtox <pmishraa24@gmail.com>
2026-03-21 02:16:57 +0530

    added

---
+++ 16/two_permutations.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n , a , b;
+        cin>>n>>a>>b;
+        
+        if(a == n && b == n){
+            cout<<"Yes"<<endl;
+        }
+        else{
+            if(a + b <= n - 2) cout<<"Yes"<<endl;
+            else cout<<"No"<<endl;
+        }
+    }
+    return 0;
+}
