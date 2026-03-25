02301356101e0dd112c48be877e1a14602e5eb21
pmtox <pmishraa24@gmail.com>
2026-03-26 00:26:13 +0530

    added

--- 21/odd_queries.cpp
+++ 21/odd_queries.cpp
@@ -1,33 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    cin >> t;
-    for (int i = 0; i < t; i++){
-        int n, q;
-        cin >> n >> q;
-        vector<int> v(n);
-        for (int j = 0; j < n; j++){
-            cin >> v[j];
-        }
-        vector<long long> prefix(n + 1, 0);
-        for (int j = 1; j <= n; j++){
-            prefix[j] = prefix[j - 1] + v[j - 1];
-        }
-        for (int j = 0; j < q; j++){
-            long long l, r, k, to_add, to_subtract, result;
-            cin >> l >> r >> k;
-            to_add = (r - l + 1) * k;
-            to_subtract = prefix[r] - prefix[l - 1];
-            result = prefix[n] - to_subtract + to_add;
-            if (result % 2 != 0){
-                cout << "YES";
-            }
-            else{
-                cout << "NO";
-            }
-            cout << endl;
-        }
-    }
-    return 0;
+
+int main() {
+	long long t;cin>>t;
+	while(t--){
+	    long long n , q;cin>>n>>q;
+	    vector<long long>arr(n + 1);
+	    for(int i = 1 ; i <= n ; i++){
+	        cin>>arr[i];
+	    }
+	    vector<long long> psum(n+1);
+	    for(int i = 1 ; i <= n ; i++){
+	        psum[i] = psum[i-1] + arr[i];
+	    }
+	    while(q--){
+	        long long l , r , k; cin>>l>>r>>k;
+	        
+	        long long st_seg = psum[l-1] + (psum[n] - psum[r]);
+	        long long ans = st_seg + k*(r - l + 1);
+	        
+	        if(ans % 2 == 1){
+	            cout<<"YES"<<endl;
+	        }else{
+	            cout<<"NO"<<endl;
+	        }
+	    }
+	}
+	return 0;
 }
