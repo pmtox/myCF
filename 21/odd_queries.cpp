a48744ff2c4099fc63ea21999f655a9394423f3c
pmtox <pmishraa24@gmail.com>
2026-03-25 23:31:12 +0530

    added

--- 21/odd_queries.cpp
+++ 21/odd_queries.cpp
@@ -1,83 +1,34 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-int main()
-
-{
-
+int main(){
     int t;
-
     cin >> t;
-
-    for (int i = 0; i < t; i++)
-
-    {
-
+    for (int i = 0; i < t; i++){
         int n, q;
-
         cin >> n >> q;
-
         vector<int> v(n);
-
-        for (int j = 0; j < n; j++)
-
-        {
-
+        for (int j = 0; j < n; j++){
             cin >> v[j];
-
         }
-
         vector<long long> prefix(n + 1, 0);
-
-        for (int j = 1; j <= n; j++)
-
-        {
-
+        for (int j = 1; j <= n; j++){
             prefix[j] = prefix[j - 1] + v[j - 1];
-
         }
-
-
-
-        for (int j = 0; j < q; j++)
-
-        {
-
+        for (int j = 0; j < q; j++){
             long long l, r, k, to_add, to_subtract, result;
-
             cin >> l >> r >> k;
-
             to_add = (r - l + 1) * k;
-
             to_subtract = prefix[r] - prefix[l - 1];
-
             result = prefix[n] - to_subtract + to_add;
-
-            if (result % 2 != 0)
-
-            {
-
+            if (result % 2 != 0){
                 cout << "YES";
-
             }
-
-            else
-
-            {
-
+            else{
                 cout << "NO";
-
             }
-
             cout << endl;
-
         }
-
     }
-
-
-
     return 0;
 
 }
