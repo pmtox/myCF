f9ccb60f88b88e833c5fa11777720aafd3d185a3
pmtox <pmishraa24@gmail.com>
2026-04-08 01:47:25 +0530

    added

--- 34/A_Helmets_in_Night_Light.cpp
+++ 34/A_Helmets_in_Night_Light.cpp
@@ -1,42 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solve(){
-    int n ,p; cin>>n>>p;
-    vector<int> ress(n); vector<int> costs(n);
-    for(int i = 0 ; i < n ; i++){
-        cin>>ress[i];
-    }
-    for(int i = 0 ; i < n ; i++){
-        cin>>costs[i];
-    }
+    int n,p;cin>>n>>p;
+    vector<int>ress(n); vector<int> costs(n);
+    for(int i = 0 ; i < n ; i++) cin>>ress[i];
+    for(int i = 0 ; i < n ; i++) cin>>costs[i];
 
     vector<pair<int , int>> arr;
     for(int i = 0 ; i < n ; i++){
-        arr.push_back({costs[i] , ress[i] });
+        arr.push_back({costs[i] , ress[i]});
     }
+
     sort(arr.begin() , arr.end());
-
-    long long fst = p;
-    int rem = n-1;
+    int ans = p;
     int idx = 0;
-    while(rem > 0 && idx < n){
-        int cost = arr[idx].first;
-        int capacity = arr[idx].second;
+    int rem = n-1;
 
-        if(cost>= p) break;
-        int take = min(rem , capacity);
-        fst+=(long long)take * cost;
-        rem -= take;
+    while(idx < n && rem > 0){
+        int cost = arr[idx].first;
+        int container = arr[idx].second;
+
+        if(cost >= p) break;
+        int lft = min(container , rem);
+        ans += (long long)lft * cost;
+        rem-=lft;
         idx++;
     }
-    fst+= (long long) rem * p;
-    
-    cout<<fst<<endl;
+    ans+=(long long) rem * p;
+    cout<<ans<<endl;
 }
 int main(){
     int t;cin>>t;
     while(t--){
         solve();
     }
-    return 0;
 }
