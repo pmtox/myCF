9566b3ec9a81bf8518751d69ab09d0f3325c82e3
pmtox <pmishraa24@gmail.com>
2026-04-09 01:28:55 +0530

    added

---
+++ 36/A_Lights_Out.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5][5];
+    memset(arr,0,sizeof(arr));
+    for(int i = 1 ; i <=3; i++){
+        for(int j = 1 ; j <=3; j++){
+            cin>>arr[i][j];
+        }
+    }
+    for(int i = 1 ; i <= 3 ; i++ , cout<<endl){
+        for(int j = 1 ; j <= 3 ; j++){
+            cout<<((arr[i][j] + arr[i+1][j] + arr[i-1][j] 
+                    + arr[i][j-1] + arr[i][j+1])  % 2 == 0);
+        }
+    }
+    return 0;
+}
